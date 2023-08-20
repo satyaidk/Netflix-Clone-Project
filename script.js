6a47dd63f2310ad36814284220b3e04b267db15b
satyaidk <nikadisatyanarayana@gmail.com>
2023-08-20 16:55:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function auth(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if(email== "netflix@gmail.com" && password=="123456789"){
+        window.location.href = "index.html";
+        alert("Login Succesfully ");
+    }
+    else{
+        alert("Login Failed");
+        return;
+    }
+}
